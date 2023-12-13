cc019bbcd96c934baafd42adfa6ae6a9667df0e2
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2023-12-14 01:38:03 +0600

    Update index.js

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -1,24 +1,63 @@
-import React from 'react'
-import ChatWindow from '../../components/ChatWindow'
+// pages/chat.js
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
 
-function index() {
+const Chat = () => {
+  const { data: session, status } = useSession();
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  useEffect(() => {
+    // Fetch messages from your server or database
+    // Example: fetchMessages();
+  }, []);
+
+  const fetchMessages = async () => {
+    // Fetch messages from your server or database
+    // Update the messages state
+  };
+
+  const handleSendMessage = async () => {
+    // Send the new message to your server or database
+    // Example: sendMessage();
+  };
+
+  const sendMessage = async () => {
+    // Send the new message to your server or database
+    // Fetch the updated messages and update the messages state
+    await fetchMessages();
+  };
+
   return (
-    <div className='justify-center'>
-      
-        
-      
-        <div class="min-h-screen flex items-center justify-center">
-    <div class="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
-      
-      
-      <div class="text-center">
-         <ChatWindow/>
-      </div>
-      <p class="text-xs text-gray-600 text-center mt-8">&copy; 2023 Shawon</p>
+    <div>
+      <h1>Messenger Chat</h1>
+      {status === 'authenticated' && (
+        <div>
+          <p>Welcome, {session.user.name}!</p>
+          <div>
+            <div style={{ maxHeight: '300px', overflowY: 'scroll' }}>
+              {messages.map((message) => (
+                <div key={message.id}>
+                  <strong>{message.sender}</strong>: {message.text}
+                </div>
+              ))}
+            </div>
+            <div>
+              <input
+                type="text"
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+              />
+              <button onClick={handleSendMessage}>Send</button>
+            </div>
+          </div>
+        </div>
+      )}
+      {status === 'unauthenticated' && (
+        <p>Please sign in to access the chat.</p>
+      )}
     </div>
-  </div>
-    </div>
-  )
-}
+  );
+};
 
-export default index
+export default Chat;
