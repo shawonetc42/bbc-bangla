5f40479000da7398947950ddf6b8ffb34a59a55d
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2023-12-13 23:51:11 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,111 @@
+// pages/index.js
+import { useEffect, useState } from 'react';
+import app from '../shared/FirebaseConfig';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  where,
+  doc,
+  getDoc,
+  updateDoc,
+} from 'firebase/firestore';
+import GameList from '../components/Home/GameList';
+import Posts from '../components/Home/Posts';
+import styles from '../../myportfoliohowto/styles/Home.module.css'; // Import CSS module
 
-import GameList from '../components/Home/GameList'
-import Posts from '../components/Home/Posts'
-import app from '../shared/FirebaseConfig'
-import { getFirestore, doc, setDoc, getDoc, 
-  collection, getDocs, query, where } from "firebase/firestore";
-import { useEffect, useState } from 'react'
-import FirrstPage from '../components/Home/FirrstPage'
-import Task from '../components/Home/Task'
-import Slide from '../components/Admin/Slide'
-import Section1bg from '../components/TechWind/Section1bg';
-import Grid from '../components/TechWind/Grid';
-import Project from '../components/TechWind/Project';
-import Footer from '../components/bbc/Footer';
-import Header from '../components/bbc/Header';
-import Header2 from '../components/bbc/Header2';
 
 export default function Home() {
+  const db = getFirestore(app);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const db=getFirestore(app);
-  const [posts,setPosts]=useState([])
-  useEffect(()=>{
-    getPost();
-  },[])
+  useEffect(() => {
+    getPosts();
+  }, []);
 
-  const getPost=async()=>{
-    const querySnapshot = await getDocs(collection(db, "admin"));
-    querySnapshot.forEach((doc) => {
-    
-   setPosts(posts=>[...posts,doc.data()]);
-});
-  }
-
-  const onGamePress=async(gameName)=>{
-    setPosts([]);
-    if(gameName=='Other Games')
-    {
-      getPost();
-      return ;
+  const getPosts = async () => {
+    try {
+      setLoading(true);
+      const querySnapshot = await getDocs(collection(db, 'admin'));
+      const postData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setPosts(postData);
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+    } finally {
+      setLoading(false);
     }
-    const q=query(collection(db,"admin"),
-    where("game","==",gameName));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      let data=doc.data();
-      data.id=doc.id
-      setPosts(posts=>[...posts,doc.data()]);
-   
-});
-  }
-  
+  };
+
+  const onGamePress = async (gameName) => {
+    setPosts([]);
+    if (gameName === 'Other Games') {
+      getPosts();
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const q = query(collection(db, 'admin'), where('game', '==', gameName));
+      const querySnapshot = await getDocs(q);
+      const postData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setPosts(postData);
+    } catch (error) {
+      console.error('Error fetching posts by game:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleLike = async (postId) => {
+    try {
+      const postRef = doc(db, 'admin', postId);
+      const postDoc = await getDoc(postRef);
+
+      if (postDoc.exists()) {
+        const currentLikes = postDoc.data().likes || 0;
+        await updateDoc(postRef, { likes: currentLikes + 1 });
+        // Fetch posts again to update the state
+        getPosts();
+      }
+    } catch (error) {
+      console.error('Error updating likes:', error);
+    }
+  };
+
+  const handleComment = async (postId, newComment) => {
+    try {
+      const postRef = doc(db, 'admin', postId);
+      const postDoc = await getDoc(postRef);
+
+      if (postDoc.exists()) {
+        const currentComments = postDoc.data().comments || [];
+        const updatedComments = [...currentComments, newComment];
+
+        await updateDoc(postRef, { comments: updatedComments });
+        // Fetch posts again to update the state
+        getPosts();
+      }
+    } catch (error) {
+      console.error('Error updating comments:', error);
+    }
+  };
+
   return (
-   <div>
-   
-   <Header/>
-    
-     <div className='flex flex-col items-center justify-center mt-9'>
-       <div className=''>
-          
-          
-          {/* <GameList onGamePress={onGamePress}/> */}
-          {posts? <Posts posts={posts} />:null}
-          
-       </div>
-     </div>
-     <Footer/>
+    <div className={styles.homeContainer}>
+      <GameList onGamePress={onGamePress} />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <Posts posts={posts} onLike={handleLike} onComment={handleComment} />
+      )}
     </div>
-  )
+  );
 }
