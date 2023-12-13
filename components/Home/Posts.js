44b1c79e0dbd8b644b17de97af3829c623eb7c3d
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2023-12-13 23:39:39 +0600

    1.1 Update Posts.js

--- components/Home/Posts.js
+++ components/Home/Posts.js
@@ -1,5 +1,6 @@
 // components/Home/Posts.js
 import React, { useState } from 'react';
+import { FaThumbsUp, FaComment } from 'react-icons/fa';
 
 const Posts = ({ posts, onLike, onComment }) => {
   const [newComment, setNewComment] = useState('');
@@ -16,8 +17,8 @@ const Posts = ({ posts, onLike, onComment }) => {
     const currentTime = getCurrentTime();
     const timestampedComment = `${newComment} - ${currentTime}`;
     onComment(postId, timestampedComment, commentType, parentId);
-    setNewComment('');
-    setReplyToIndex(null);
+    setNewComment(''); // Clear the comment input after submitting
+    setReplyToIndex(null); // Reset replyToIndex
   };
 
   const toggleComments = () => {
@@ -29,86 +30,103 @@ const Posts = ({ posts, onLike, onComment }) => {
   };
 
   const loadMoreComments = () => {
-    setCommentsToShow((prev) => prev + 3);
+    setCommentsToShow((prev) => prev + 3); // Increase the number of comments to show by 3
   };
 
+  // Sort posts based on the number of likes in descending order
+  const sortedPosts = posts.slice().sort((a, b) => b.likes - a.likes);
+
   return (
-    <div className="flex flex-wrap">
-      {posts.map((post, postIndex) => (
-        <div key={post.id} className="border p-4 m-4 w-full max-w-md">
-          <h2 className="text-lg font-semibold">{post.title}</h2>
+    <div className="flex flex-col space-y-4">
+      {sortedPosts.map((post, postIndex) => (
+        <div key={post.id} className="border p-4 rounded-md bg-white shadow-md">
+          <h2 className="text-lg font-semibold mb-2">{post.title}</h2>
           {post.image && (
-            <img src={post.image} alt={post.title} className="w-full h-32 object-cover mt-2 rounded" />
+            <img src={post.image} alt={post.title} className="w-full h-48 object-cover rounded" />
           )}
-          <p>{post.content}</p>
-          <div className="flex justify-between mt-2">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => onLike(post.id)}>
-              Like
-            </button>
-            <span className="text-gray-600">Likes: {post.likes || 0}</span>
-            <span className="text-gray-600">Comments: {post.comments ? post.comments.length : 0}</span>
-          </div>
-          <div className="mt-2">
-            <input
-              type="text"
-              placeholder="Tweet your reply..."
-              value={newComment}
-              onChange={(e) => setNewComment(e.target.value)}
-              className="border p-2 w-full"
-            />
+          <p className="text-gray-800 mb-4">{post.content}</p>
+          <div className="flex items-center text-gray-500 space-x-4">
             <button
-              onClick={() => handleComment(post.id, 'comment')}
-              className="bg-blue-500 text-white px-4 py-2 rounded mt-2"
+              className="flex items-center cursor-pointer"
+              onClick={() => onLike(post.id)}
             >
-              Tweet
+              <FaThumbsUp className="mr-1" />
+              Like {post.likes || 0}
+            </button>
+            
+            <button
+              className="flex items-center cursor-pointer"
+              onClick={() => toggleComments()}
+            >
+              <FaComment className="mr-1" />
+              Comment {post.comments ? post.comments.length : 0}
             </button>
           </div>
-          <div className={`mt-2 ${showComments ? 'block' : 'hidden'}`}>
-            {post.comments &&
-              post.comments.slice(0, commentsToShow).map((comment, commentIndex) => (
-                <div
-                  key={commentIndex}
-                  className={`bg-gray-100 p-2 rounded mt-2 ${replyToIndex === commentIndex ? 'ml-8' : ''}`}
-                >
-                  {/* Twitter-like UI for each comment */}
-                  <div className="flex items-start">
-                    <img src={comment.userAvatar} alt="Avatar" className="w-8 h-8 object-cover rounded-full mr-2" />
+          <div className={`mt-4 ${showComments ? 'block' : 'hidden'}`}>
+            {/* Comment Box */}
+            <div className="flex items-center space-x-4 mt-2">
+              <input
+                type="text"
+                placeholder="Write a comment..."
+                value={newComment}
+                onChange={(e) => setNewComment(e.target.value)}
+                className="border p-2 w-full rounded"
+              />
+              <button
+                onClick={() => handleComment(post.id, 'comment')}
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >
+                Comment 
+              </button>
+            </div>
+            {/* Comments */}
+            <div className="mt-2 text-gray-600">
+              {post.comments &&
+                post.comments.slice(0, commentsToShow).map((comment, commentIndex) => (
+                  <div
+                    key={commentIndex}
+                    className={`flex items-start bg-gray-100 p-2 rounded mt-2 ${
+                      replyToIndex === commentIndex ? 'ml-8' : ''
+                    }`}
+                  >
+                    <div className="flex-shrink-0 w-8 h-8 bg-gray-200 rounded-full mr-2"></div>
                     <div>
-                      <span className="font-semibold">{comment.userName}</span>
-                      <span className="text-gray-600">@{comment.userHandle}</span>
-                      <p>{comment.text}</p>
-                      <div className="flex items-center mt-2">
-                        <button onClick={() => toggleReplyForm(commentIndex)} className="text-blue-500 mr-2">
-                          Reply
-                        </button>
-                        <span className="text-gray-600">{comment.timestamp}</span>
-                      </div>
+                      <p className="text-gray-700">{comment}</p>
+                      <button
+                        onClick={() => toggleReplyForm(commentIndex)}
+                        className="text-blue-500 mt-1"
+                      >
+                        Reply
+                      </button>
                       {replyToIndex === commentIndex && (
                         <div className="mt-2">
                           <input
                             type="text"
-                            placeholder="Tweet your reply..."
+                            placeholder="Write a reply..."
                             value={newComment}
                             onChange={(e) => setNewComment(e.target.value)}
-                            className="border p-2 w-full"
+                            className="border p-2 w-full rounded"
                           />
                           <button
                             onClick={() => handleComment(post.id, 'reply', commentIndex)}
                             className="bg-blue-500 text-white px-4 py-2 rounded mt-2"
                           >
-                            Tweet
+                            Reply
                           </button>
                         </div>
                       )}
                     </div>
                   </div>
+                ))}
+              {post.comments && post.comments.length > commentsToShow && (
+                <div className="text-blue-500 cursor-pointer" onClick={loadMoreComments}>
+                  {showComments ? 'Show More Comments' : 'Hide Comments'}
                 </div>
-              ))}
-            {post.comments && post.comments.length > commentsToShow && (
-              <div className="mt-2 text-blue-500 cursor-pointer" onClick={loadMoreComments}>
-                {showComments ? 'Show More Comments' : 'Hide Comments'}
+              )}
+              <div className="mt-2">
+                
               </div>
-            )}
+            </div>
           </div>
         </div>
       ))}
