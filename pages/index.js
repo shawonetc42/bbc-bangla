3ced8e257bd04337183e0465a974a0da476daaac
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2023-12-13 23:58:58 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ import {
 } from 'firebase/firestore';
 import GameList from '../components/Home/GameList';
 import Posts from '../components/Home/Posts';
-import styles from '../../styles/Home.module.css'; // Import CSS module
+import styles from '../../myportfoliohowto/styles/Home.module.css'
 
 
 export default function Home() {
