6781a28fff1b6b4363172adbef03f6e683f4b3b1
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2023-12-14 17:22:54 +0600

    Update Data.js

--- shared/Data.js
+++ shared/Data.js
@@ -7,27 +7,27 @@ const GameList=[
     {
         id:2,
         name:'সংসদ নির্বাচন ২০২৪',
-        image:'./3.png'
+        image:'https://t3.ftcdn.net/jpg/03/07/93/88/360_F_307938835_NChzYE26DIyfzHdAdW722BTaOnjaHSqV.jpg'
     },
     {
         id:3,
         name:'বাংলাদেশ',
-        image:'./2.png'
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6F_RSdK7hBs3PWYHW8bw-uhfvFLeR3VuSDA&usqp=CAU'
     },
     {
         id:4,
         name:'ভারত',
-        image:'./4.png'
+        image:'https://media.istockphoto.com/id/1310940729/vector/live-stream-icon-vector-design-element-play-button-for-news-and-television-or-online.jpg?s=612x612&w=0&k=20&c=uaYKSItjlO74Js6rMlAGkhH07uI0MSfoQwzinWgqJUo='
     },
     {
         id:5,
         name:'বিশ্ব',
-        image:'./5.png'
+        image:'https://content.wepik.com/statics/27178511/preview-page0.jpg'
     },
     {
         id:6,
         name:'স্বাস্থ্য',
-        image:'./6.png'
+        image:'https://st2.depositphotos.com/3326513/7482/v/450/depositphotos_74826231-stock-illustration-tv-channel-microphone-with-news.jpg'
     },
     {
         id:7,
